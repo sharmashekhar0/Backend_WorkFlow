893b2c7311e94b8bb6183a35206f8759db072adb
sharmashekhar0 <beingperfect76@gmail.com>
2024-02-25 11:16:10 +0530

    add basic setup files

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+const DB_NAME = "Backend_WorkFlow";
+
+const CLOUDINARY_CLOUD_NAME = String(process.env.CLOUDINARY_CLOUD_NAME);
+const CLOUDINARY_API_KEY = String(process.env.CLOUDINARY_API_KEY);
+const CLOUDINARY_API_SECRET = String(process.env.CLOUDINARY_API_SECRET);
+
+export {
+    DB_NAME,
+    CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET,
+};
